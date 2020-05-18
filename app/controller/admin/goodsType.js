6623d7a64174e2ea8505f1a85b3ff0c83fbaef0e
钱立峰 <805042956@qq.com>
2020-05-18 21:19:54 +0800

    fix:59

---
+++ app/controller/admin/goodsType.js
@@ -0,0 +1,67 @@
+var BaseController = require('./base.js');
+class GoodsTypeController extends BaseController {
+  async index() {
+
+    //查询商品类型表
+    var result = await this.ctx.model.GoodsType.find({});
+    await this.ctx.render('admin/goodsType/index', {
+
+      list: result
+    });
+  }
+
+
+  async add() {
+
+
+    await this.ctx.render('admin/goodsType/add');
+
+  }
+
+  async doAdd() {
+
+    //  console.log(this.ctx.request.body);
+
+    var res = new this.ctx.model.GoodsType(this.ctx.request.body)
+
+    await res.save();   //注意
+
+    await this.success('/admin/goodsType', '增加类型成功');
+
+
+  }
+
+
+
+
+  async edit() {
+
+
+    var id = this.ctx.query.id;
+
+    var result = await this.ctx.model.GoodsType.find({ "_id": id });
+
+    await this.ctx.render('admin/goodsType/edit', {
+
+      list: result[0]
+    });
+
+  }
+
+  async doEdit() {
+
+
+
+    var _id = this.ctx.request.body._id;
+    var title = this.ctx.request.body.title;
+    var description = this.ctx.request.body.description;
+
+    await this.ctx.model.GoodsType.updateOne({ "_id": _id }, {
+      title, description
+    })
+    await this.success('/admin/goodsType', '编辑类型成功');
+
+  }
+
+}
+module.exports = GoodsTypeController;
--- app/router.js
+++ app/router.js
@@ -44,12 +44,16 @@ module.exports = app => {
   router.post('/admin/access/doEdit', controller.admin.access.doEdit);
   router.get('/admin/access/delete', controller.admin.base.delete);
 
-  //上传图片演示
-
   router.get('/admin/focus', controller.admin.focus.index);
   router.get('/admin/focus/add', controller.admin.focus.add);
   router.post('/admin/focus/doAdd', controller.admin.focus.doAdd);
   router.get('/admin/focus/edit', controller.admin.focus.edit);
   router.post('/admin/focus/doEdit', controller.admin.focus.doEdit);
+
+  router.get('/admin/goodsType', controller.admin.goodsType.index);
+  router.get('/admin/goodsType/add', controller.admin.goodsType.add);
+  router.get('/admin/goodsType/edit', controller.admin.goodsType.edit);
+  router.post('/admin/goodsType/doEdit', controller.admin.goodsType.doEdit);
+  router.post('/admin/goodsType/doAdd', controller.admin.goodsType.doAdd);
 
 };
