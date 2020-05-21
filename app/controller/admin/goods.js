1ff6385eb4b1f9adc1ad95fdc1131bd64d359a37
钱立峰 <805042956@qq.com>
2020-05-21 22:38:41 +0800

    fix:67

--- app/controller/admin/goods.js
+++ app/controller/admin/goods.js
@@ -1,18 +1,55 @@
 'use strict';
 
-var BaseController =require('./base.js');
+var BaseController = require('./base.js');
 
 class GoodsController extends BaseController {
-      async index() {       
-      
-          await this.ctx.render('admin/goods/index');
-      }     
-    
-      async add() {
-      
-        await this.ctx.render('admin/goods/add');
-        
-      }     
+  async index() {
+
+    await this.ctx.render('admin/goods/index');
+  }
+
+  async add() {
+
+    //获取所有的颜色值
+    var colorResult = await this.ctx.model.GoodsColor.find({});
+
+    //获取所有的商品类型
+    var goodsType = await this.ctx.model.GoodsType.find({});
+
+
+    await this.ctx.render('admin/goods/add', {
+
+      colorResult: colorResult,
+      goodsType: goodsType
+    });
+
+  }
+
+  async doAdd() {
+
+
+    console.log(this.ctx.request.body);
+
+  }
+
+
+  //获取商品类型的属性 api接口
+  async goodsTypeAttribute() {
+
+
+    var cate_id = this.ctx.request.query.cate_id;
+
+    //注意 await
+    var goodsTypeAttribute = await this.ctx.model.GoodsTypeAttribute.find({ "cate_id": cate_id })
+
+    console.log(goodsTypeAttribute);
+
+    this.ctx.body = {
+      result: goodsTypeAttribute
+    }
+
+  }
+
 
 }
 
