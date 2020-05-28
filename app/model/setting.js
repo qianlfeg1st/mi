c06127ee5189ded9acae332133a838b4a295a10f
钱立峰 <805042956@qq.com>
2020-05-28 22:36:24 +0800

    fix:82

--- app/model/setting.js
+++ app/model/setting.js
@@ -1,33 +1,33 @@
 module.exports = app => {
-    const mongoose = app.mongoose;
-    const Schema = mongoose.Schema;
-          
-    const SettingSchema = new Schema({
-      site_title: { type: String  },
-      site_logo: { type: String  },
-      site_keywords: {  
-        type: String 
-      },   
-      site_description:{
-        type: String 
-      },
-      no_picture:{
-        type: String    
-      },
-      site_icp:{     
-        type: String    
-      },      
-      site_tel: {
-        type: String       
-      },    
-      search_keywords: {           
-        type: String      
-      },  
-      tongji_code: {           
-        type: String    
-      }       
+  const mongoose = app.mongoose;
+  const Schema = mongoose.Schema;
 
-    });
-   
-    return mongoose.model('Setting', SettingSchema,'setting');
+  const SettingSchema = new Schema({
+    site_title: { type: String },
+    site_logo: { type: String },
+    site_keywords: {
+      type: String
+    },
+    site_description: {
+      type: String
+    },
+    no_picture: {
+      type: String
+    },
+    site_icp: {
+      type: String
+    },
+    site_tel: {
+      type: String
+    },
+    search_keywords: {
+      type: String
+    },
+    tongji_code: {
+      type: String
+    }
+
+  });
+
+  return mongoose.model('Setting', SettingSchema, 'setting');
 }
